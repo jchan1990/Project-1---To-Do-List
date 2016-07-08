4c86619d1ec8afa0f6e1e67d163fbcedcf2a9947
Jason Chan <jchan122490@gmail.com>
2016-07-08 16:47:54 -0400

    Not complete going to work over weekend... sad face

---
+++ MyApplication/app/src/main/java/com/example/qube/to_dolist/ToDoListSingleton.java
@@ -0,0 +1,12 @@
+package com.example.qube.to_dolist;
+
+/**
+ * Created by Qube on 7/8/16.
+ */
+public class ToDoListSingleton {
+    private static ToDoListSingleton mToDoListSingleton;
+
+
+
+
+}
